4c09af7c3ebe9794d8f4023595b5a27d1eac284c
Aaron Arney <github@ocular-rhythm.com>
2019-12-18 12:46:03 -0500

    Initialize and setup the plugin

---
+++ includes/ferret.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ferret;
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+$options = new Options();
+$loader = new Loader();
+
+if ( is_admin() ) {
+    $plugin_admin = new Admin( $options );
+    $loader->add_action( 'admin_menu', $plugin_admin, 'add_admin_menu' );
+    $loader->add_action( 'admin_init', $plugin_admin, 'settings_init' );
+} else {
+    $plugin_client = new Client( $options );
+    $loader->add_action( 'wp_enqueue_scripts', $plugin_client, 'enqueue_scripts' );
+}
+
+$sentry = new SentryAdapter( $options );
+$loader->run();
